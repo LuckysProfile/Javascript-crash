5f55db276ee136f78fd4c80fcce131416c6b21e5
Harsh <143166692+LuckysProfile@users.noreply.github.com>
2025-09-14 10:50:51 +0000

    variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,10 @@
+"use script"; // Treat all JS code as newer version
+
+// alert(3+3)               //runs in browser, not in node js
+
+// learning best beginner practices 
+
+
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 1234;
+
+let emailId = "harsh@123gmail.com";
+
+var accountPass = "12345"; // please don't use var now in javascript
+
+
+
+// accountId = 2;
+
+emailId = "Hello@123gmail.com";
+
+// console.log(emailId);
+
+console.table([accountId, emailId, accountPass]);
+
+
+
+
