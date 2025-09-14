1708a7e6d439f7d4db387e60a1c675c428e19676
Harsh <143166692+LuckysProfile@users.noreply.github.com>
2025-09-14 12:09:37 +0000

    conversions

---
+++ basics/conversionOperations.js
@@ -0,0 +1,16 @@
+let score = "10";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+let value = 3;
+
+let negValue = -value;
+
+console.log(negValue);
+
+
